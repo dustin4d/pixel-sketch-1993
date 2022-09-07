c3a8a6c1b942b0e1d281d1146b5ddb0a12064608
Dustin4d <dustinholder14@yahoo.com>
2022-09-07 14:12:55 -0400

    Changed divs to canvas elements

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-console.log("javascript linked")
-
 const container = document.querySelector('.container')
 
 function addSquare() {
-    const newSquare = document.createElement('div')
-    const squareContent = document.createTextNode('New Square')
+    const newSquare = document.createElement('canvas')
+    const squareContent = document.createTextNode('<canvas></canvas>')
     newSquare.appendChild(squareContent)
 
-    container.appendChild(squareContent)
+    container.appendChild(newSquare)
+    newSquare.classList.add('square')
 }
 
 for (let i = 0; i <= 16; i++){
