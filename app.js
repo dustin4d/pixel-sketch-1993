784022ded1a827c473970b9203af81298817e0bb
Dustin4d <dustinholder14@yahoo.com>
2022-09-09 05:03:14 -0400

    Style overhaul

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const gridContainer = document.querySelector('.grid-container')
 function addSquare() {
     const canvas = document.createElement('div')
     gridContainer.appendChild(canvas)
-    canvas.classList.add('')
+    canvas.classList.add('.grid-cell')
 }
 
 for (let i = 0; i <= 32; i++){
