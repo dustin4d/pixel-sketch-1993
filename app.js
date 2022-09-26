56e0c3b4c59b8d944dfbd573662be83bb72a3008
dustin4d <dustinholder14@yahoo.com>
2022-09-26 15:32:46 -0500

    Color bar now returns to toolbar when clicked

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Constants
 const gridCell = document.querySelector('.grid-cell')
 const gridContainer = document.querySelector('.grid-container')
 const resolutionTag = document.querySelector('.resolution-tag')
@@ -8,12 +7,13 @@ const buttons = document.querySelector('button')
 const colorBtn = document.querySelector('#colorBtn')
 const returnBtn = document.querySelector('.return-button')
 const palette = document.querySelectorAll('.palette')
+const cells = document.querySelectorAll('.grid-cell')
 
 // Current pen color
-let currentColor
+let currentColor = 'rgb(0, 0, 255)'
 
-// Event Listeners
 
+// Toggle between two bars
 colorBtn.addEventListener('click', function() {
     // Hide the toolbar
     toolbar.classList.remove('show')
@@ -24,17 +24,28 @@ colorBtn.addEventListener('click', function() {
     colorbar.classList.remove('hidden')
 })
 
+
+// Basic event listener attached to color options
 for (const pickedColor of palette) {
     pickedColor.addEventListener('click', function(){
         console.log('Color picked!')
+
+        // Revert back to toolbar
+        colorbar.classList.remove('show')
+        colorbar.classList.add('hidden')
+
+        toolbar.classList.remove('hidden')
+        toolbar.classList.add('show')
     })
 }
 
-// Logic
 function addSquare() {
     const canvas = document.createElement('div')
     gridContainer.appendChild(canvas)
     canvas.classList.add('grid-cell')
+    canvas.addEventListener('click', function(){
+        canvas.style.backgroundColor = currentColor
+    })
 }
 
 let currentRes = 16;
@@ -42,11 +53,3 @@ let currentRes = 16;
 for (let i = 0; i < currentRes ** 2; i++){
     addSquare();
 }
-
-function swapBars() {
-    buttons.remove();
-
-    const colorButton = document.createElement('button')
-    toolbar.appendChild(colorButton)
-}
-
